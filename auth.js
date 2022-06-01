02c7abbf0aa470fb28b81e0e18091968f129dad6
yghonem14 <yghonem14>
2022-06-02 01:08:59 +0200

    k

--- auth.js
+++ auth.js
@@ -1,8 +1,11 @@
 const jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
   try {
-    const nonSecurePaths = ['/api/users/login','/api/users/register', '/api/keys/check','/api/updates/urge'];
-  if (nonSecurePaths.includes(req.path)) return next();
+    const nonSecurePaths = ['/api/users/login','/api/users/register', '/api/keys/check','/api/updates'];
+  if (nonSecurePaths.includes(req.path)){
+     console.log(nonSecurePaths.includes(req.path),req.path)
+     return next();
+    }
     const token = req.headers.authorization.replace("Bearer ", "");
     const decoded = jwt.verify(token, process.env.JWT);
     req.userData = decoded;
